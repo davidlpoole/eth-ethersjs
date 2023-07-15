b8b0c6e31ad768ab15ff9c651bea88ae378434a9
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-07-15 20:12:13 +0100

    Instantiate 2 wallets (private key vs mnemonic phrase)

---
+++ wallets.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const ethers = require('ethers');
+const { Wallet } = ethers;
+
+// create a wallet with a private key
+const wallet1 = new Wallet(process.env.PRIVATE_KEY);
+
+// create a wallet from mnemonic
+const wallet2 = Wallet.fromPhrase(process.env.PRIVATE_PHRASE);
+
+module.exports = {
+    wallet1,
+    wallet2,
+}
