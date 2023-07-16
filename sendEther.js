087912a9c743060b68898faa62b91b99ae35d008
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-07-16 09:28:30 +0100

    Link the provider to the wallet, then use sendTransaction instead of sign

--- sendEther.js
+++ sendEther.js
@@ -1,19 +1,19 @@
-const { Wallet, utils, providers } = require('ethers');
+const { Wallet, providers } = require('ethers');
 const { ganacheProvider, PRIVATE_KEY } = require('./config');
 
 // Define the web3 provider
 const provider = new providers.Web3Provider(ganacheProvider);
-const wallet = new Wallet(PRIVATE_KEY);
+const wallet = new Wallet(PRIVATE_KEY, provider);
 
 async function sendEther({ value, to }) {
-    const rawTx = await wallet.signTransaction({
+    const rawTx = await wallet.sendTransaction({
         value, to,
         gasLimit: 0x5208,
         gasPrice: 0x3b9aca00
     });
 
-    // TODO: send the transaction and return the transaction promise
-    return provider.sendTransaction(rawTx)
+    // send the transaction and return the transaction promise
+    return rawTx
 }
 
 module.exports = sendEther;
